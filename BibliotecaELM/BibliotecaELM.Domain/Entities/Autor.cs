662520ae49ecc15e6fbdd5d33ceee7684d154db6
lulzera <lubarrosg2006@gmail.com>
2026-03-14 16:49:14 -0300

    feat: finalizando classes Autor.cs e Compra.cs

--- BibliotecaELM/BibliotecaELM.Domain/Entities/Autor.cs
+++ BibliotecaELM/BibliotecaELM.Domain/Entities/Autor.cs
@@ -1,10 +1,28 @@
 ﻿using System.Net.Mime;
+using BibliotecaELM.Domain.Common;
 
 namespace BibliotecaELM.Domain.Entities;
 
-public class Autor
+public class Autor : BaseEntity
 {
-    public long id_autor { get; private set; }
     public string nome_autor { get; private set; }
-    public DateTime nascimento { get; private set; }
+    public DateOnly nascimento { get; private set; }
+
+    public Autor(string nome, DateOnly nascimento)
+    {
+        this.nome_autor = nome;
+        ValidateLancamento(nascimento);
+    }
+
+    public void ValidateLancamento(DateOnly data)
+    {
+        if (data.Year < 2015)
+        {
+            nascimento = data;
+        }
+        else
+        {
+            throw new Exception("insira um valor válido.");
+        }
+    }
 }
--- BibliotecaELM/BibliotecaELM.Domain/Entities/Compra.cs
+++ BibliotecaELM/BibliotecaELM.Domain/Entities/Compra.cs
@@ -2,8 +2,40 @@
 
 public class Compra
 {
-    public long id_compra  { get; private set; }
     public string forma_compra { get; private set; }
-    public DateTime data_compra { get; private set; }
-    public long id_user { get; private set; }
+    public DateOnly data_compra { get; private set; }
+    
+    public Usuario Usuario { get; private set; }
+
+    public Compra(string forma_compra, DateOnly data_compra, Usuario usuario)
+    {
+        validateFormaCompra(forma_compra);
+        validateDataCompra(data_compra);
+        this.Usuario = usuario;
+    }
+
+    public void validateFormaCompra(string forma)
+    {
+        if (forma.ToLower() == "debito" || forma.ToLower() == "credito" || forma.ToLower() == "dinheiro" ||
+            forma.ToLower() == "pix")
+        {
+            forma_compra = forma;
+        }
+        else
+        {
+            throw new Exception("Insira um valor válido.");
+        }
+    }
+
+    public void validateDataCompra(DateOnly data)
+    {
+        if (data.Year < 1900 && data.Year > DateTime.Now.Year)
+        {
+            throw new Exception("Insira um valor valido.");
+        }
+        else
+        {
+            data_compra = data;
+        }
+    }
 }
